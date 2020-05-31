7da93864e5b51a1c7fb35f6d2978c25f00096f2a
github username <github email address>
2020-05-30 23:58:45 -0400

    second commit

---
+++ app.js
@@ -0,0 +1,22 @@
+document.getElementById('button').addEventListener('click',
+loadData);
+
+function loadData(){
+  // Create an XHR Object
+  const xhr = new XMLHttpRequest();
+
+  // OPEN
+  xhr.open('GET', 'data.txt', true);
+
+
+  xhr.onload = function(){
+    if(this.status === 200){
+      console.log(this.resonseText);
+    } else {
+      console.log('it didnt work!');
+    }
+  }
+  xhr.send();
+}
+
+console.log('trying to set up a local server');
