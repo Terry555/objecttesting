be6b868e420581d0387fc996f78fd73e6e40c123
github username <github email address>
2020-07-02 00:55:11 -0400

    finished lecture on destructuring

--- app.js
+++ app.js
@@ -1,69 +1,36 @@
 
-const data = [
-  {
-    name: 'John Doe',
-    age: 32,
-    gender: 'male',
-    lookingfor: 'female',
-    location: 'Boston MA',
-    image: 'https://randomuser.me/api/portraits/men/82.jpg'
-  },
-  {
-    name: 'Jen Smith',
-    age: 26,
-    gender: 'female',
-    lookingfor: 'male',
-    location: 'Miami FL',
-    image: 'https://randomuser.me/api/portraits/women/82.jpg'
-  },
-  {
-    name: 'William Johnson',
-    age: 38,
-    gender: 'male',
-    lookingfor: 'female',
-    location: 'Lynn MA',
-    image: 'https://randomuser.me/api/portraits/men/83.jpg'
+let a, b
+
+// Rest pattern
+[a, b, ...rest] = [100, 200, 300, 400, 500]
+
+// Array destructuring
+// const people = ['John', 'Beth', 'Mike']
+//
+// const [person1, person2, person3] = people
+//
+// console.log(person2);
+
+// Parse array returned from function
+// function getPeople(){
+//   return ['John', 'Beth', 'Mike']
+// }
+//
+// let person1, person2, person3
+// [person1, person2, person3] = getPeople()
+// console.log(person2);
+
+const person = {
+  name: 'John Doe',
+  age: 32,
+  city: 'Miami',
+  gender: 'Male',
+  sayHello: function(){
+    console.log('Hello');
   }
-];
-
-const profiles = profileIterator(data);
-
-// Call first profile
-nextProfile();
-
-// Next Event
-document.getElementById('next').addEventListener('click', nextProfile)
-
-// Next Profile Display
-function nextProfile(){
-  const currentProfile = profiles.next().value
-
-  if(currentProfile !== undefined){
-  document.getElementById('profileDisplay').innerHTML = `
-    <ul class="list-group">
-      <li class="list-group-item">Name: ${currentProfile.name}</li>
-      <li class="list-group-item">Age: ${currentProfile.age}</li>
-      <li class="list-group-item">Location: ${currentProfile.location}</li>
-      <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
-    </ul>
-  `
-
-  document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`
-} else {
-  // No more profiles
-  window.location.reload();
-}
 }
 
-// Profile Iterator
-function profileIterator(profiles){
-  let nextIndex = 0
+// Old ES5
+const { name, age, city, sayHello } = person
 
-  return {
-    next: function(){
-      return nextIndex < profiles.length ?
-             { value: profiles[nextIndex++], done: false } :
-             { done: true }
-    }
-  }
-};
+sayHello();
